c4fbeb07726d20bc8dc2c7c8bbbfdeb29790627c
JonatanMendez349 <jonatanmendezpujols@gmail.com>
2025-04-30 01:35:10 +0000

    base

---
+++ db.py
@@ -0,0 +1,43 @@
+import psycopg2
+import os
+
+def init_db():
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            cedula TEXT UNIQUE NOT NULL,
+            email TEXT UNIQUE NOT NULL,
+            phone TEXT UNIQUE NOT NULL
+        )
+    ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS cards (
+            id SERIAL PRIMARY KEY,
+            user_id INTEGER,
+            card_id TEXT UNIQUE NOT NULL,
+            balance REAL DEFAULT 0.0,
+            FOREIGN KEY (user_id) REFERENCES users(id)
+        )
+    ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS transactions (
+            id SERIAL PRIMARY KEY,
+            user_id INTEGER,
+            card_id TEXT,
+            amount REAL NOT NULL,
+            payment_method TEXT NOT NULL,
+            transaction_id TEXT NOT NULL,
+            status TEXT NOT NULL,
+            created_at TIMESTAMP NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES users(id),
+            FOREIGN KEY (card_id) REFERENCES cards(card_id)
+        )
+    ''')
+    db.commit()
+    cursor.close()
+
+def get_db():
+    conn = psycopg2.connect(os.getenv('DATABASE_URL'))
+    return conn
